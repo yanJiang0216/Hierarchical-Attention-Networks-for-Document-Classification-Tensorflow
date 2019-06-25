25d56a392a258a01e60e939baa0971f412357860
TieDanCuihua <41666768+TieDanCuihua@users.noreply.github.com>
2019-06-25 16:07:39 +0800

    Update components.py

--- components.py
+++ components.py
@@ -39,9 +39,9 @@ def get_sentence(vocabulary_inv, sen_index):
     return ' '.join([vocabulary_inv[index] for index in sen_index])
 
 def sequence(rnn_inputs, hidden_size, seq_lens):
-    cell_fw = tf.nn.rnn_cell.GRUCell(hidden_size)
+    cell_fw = tf.nn.rnn_cell.BasicLSTMCell(hidden_size)
     print('build fw cell: '+str(cell_fw))
-    cell_bw = tf.nn.rnn_cell.GRUCell(hidden_size)
+    cell_bw = tf.nn.rnn_cell.BasicLSTMCell(hidden_size)
     print('build bw cell: '+str(cell_bw))
     rnn_outputs, final_state = tf.nn.bidirectional_dynamic_rnn(cell_fw, 
                                                                cell_bw, 
